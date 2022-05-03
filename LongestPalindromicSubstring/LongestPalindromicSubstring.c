aa4a0dfdf9e1d37b4d0f1b5b739d80b67ed5347e
Ganesh Kamath <ganeshkamath89@gmail.com>
2022-05-03 10:10:50 +0530

    extracted functions and optimized code

--- LongestPalindromicSubstring/LongestPalindromicSubstring.c
+++ LongestPalindromicSubstring/LongestPalindromicSubstring.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <malloc.h>
 
-void printSubStr(const char* str, int low, int high)
+void printSubStr(const char* str, int start, int window)
 {
-	for (int i = low; i <= high; ++i)
-	{
-		printf("%c", str[i]);
+	char *dest = (char*)calloc((window + 1), sizeof(char)); // Include Null termination
+	strncpy(dest, (str + start), window);
+	printf("%s", dest);
+	free(dest);
+}
+
+bool isSubstringPalindrome(const char* str, int start, int end, int len)
+{
+	for (int k = 0; k < len / 2; k++) {
+		if (str[start + k] != str[end - k]) {
+			return false; // Not a Palindrome
+		}
 	}
+	return true;
 }
 
 int longestPalSubstr(const char* str)
 {
 	unsigned int n = strlen(str);
-	int maxLength = 1, start = 0, len = 0, end = 0;
+	int maxLength = 1, start = 0, window = 0, end = 0;
 
-	// Nested loop to mark start and end index
+	// Nested loop to find start and maxLength
 	for (unsigned int i = 0; i < n; i++) {
 		for (unsigned int j = i; j < n; j++) {
-			int flag = 1;
-
-			// Check palindrome
-			len = j - i + 1;
-			for (int k = 0; k < len / 2; k++) {
-				if (str[i + k] != str[j - k]) {
-					flag = 0;
-					break;
+			window = j - i + 1;
+			if (isSubstringPalindrome(str, i, j, window))
+			{
+				if ((window > maxLength))
+				{
+					maxLength = window;
+					start = i;
 				}
-			}
-
-			if (flag && (len > maxLength)) {
-				maxLength = len;
-				start = i;
-				end = j;
-				printf("\nPalindrome substring: ");
-				printSubStr(str, start, end);
 			}
 		}
 	}
-	end = start + maxLength - 1;
 	printf("\n\nLongest palindrome substring is: ");
-	printSubStr(str, start, end);
+	printSubStr(str, start, maxLength);
 	return maxLength;
 }
 
